0f3ff5cf9233afeca06a4368f5443e613c77094b
jitendra kumar <jitendrakv11@gmail.com>
2022-03-02 17:36:26 +0530

    first commit

---
+++ src/main/java/com/smart/controllers/ForgetController.java
@@ -0,0 +1,28 @@
+package com.smart.controllers;
+
+import java.util.Random;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ForgetController {
+
+	@GetMapping("/forgot")
+	public String openEmailForm() {
+		
+		return "forgot_email_form";
+	}
+	
+	@PostMapping("/sendOtp")
+	public String sendOpt(@RequestParam("email") String email) {
+		Random random=new Random(1000);
+		int otp=random.nextInt(9999);
+		System.out.println("OTP"+otp);
+		//write the code to email for otp
+		return "veryfryOTP";
+	}
+}
